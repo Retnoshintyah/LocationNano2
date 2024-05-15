23d581edae122e62fe9b77478607aaf37d448e4d
retno <retno.hariyani@binus.ac.id>
2024-05-15 14:15:30 +0700

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Retno Shintya Hariyani on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
